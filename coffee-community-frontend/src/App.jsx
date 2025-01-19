dc34350971a5054dcb96dadb81a5627cd64c11fe
nazie23 <edroos.nazia@gmail.com>
2025-01-19 09:46:36 -0800

    added forum feature

--- coffee-community-frontend/src/App.jsx
+++ coffee-community-frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Search from "./pages/Search";
+import Forum from "./pages/Forum"
 import './App.css';
 
 const App = () => {
@@ -10,10 +11,12 @@ const App = () => {
       <nav class="navbar">
         <a href="/">Home</a>
         <a href="/search">Search Cafes</a>
+        <a href="/forum">Forum</a>
       </nav>
       <Routes>
         <Route path="/" element={<h1>Welcome to Third Place!</h1>} />
         <Route path="/search" element={<Search />} />
+        <Route path="/forum" element={<Forum />} />
       </Routes>
     </Router>
   );
