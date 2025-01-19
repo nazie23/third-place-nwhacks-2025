84be136d2a60b329e1cdb3af7c42776ffc03056e
nikipra16 <142063028+nikipra16@users.noreply.github.com>
2025-01-19 10:20:43 -0800

    added

--- coffee-community-frontend/src/App.jsx
+++ coffee-community-frontend/src/App.jsx
@@ -10,17 +10,19 @@ import Account from "./pages/Account";
 const App = () => {
   return (
     <Router>
+      <div className="tabs-container"></div>
       <nav class="navbar">
         <a href="/">Home</a>
         <a href="/search">Search Cafes</a>
         <a href="/account">Account</a>
         <a href="/forum">Forum</a>
       </nav>
+      <div className="tabs-container"></div>
       <Routes>
         <Route path="/" element={<h1>Welcome to Third Place!</h1>} />
         <Route path="/search" element={<Search />} />
         <Route path="/account" element={<Account />} />
-        <Route path="/cafe/:cafeId" element={<CafeDetail />} />
+        /* <Route path="/cafe/:cafeId" element={<CafeDetail />} /> */
         <Route path="/forum" element={<Forum />} />
       </Routes>
     </Router>
